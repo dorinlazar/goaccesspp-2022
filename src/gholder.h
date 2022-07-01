1812284221474b3a427c19a6dceec31b9b1bcb35
Dorin Lazăr <dorin.lazar@gmail.com>
2022-07-01 19:19:02 +0300

    nitpicking

--- src/gholder.h
+++ src/gholder.h
@@ -27,11 +27,10 @@
  * SOFTWARE.
  */
 
-#ifndef GHOLDER_H_INCLUDED
-#define GHOLDER_H_INCLUDED
+#pragma once
 
-#define MTRC_ID_COUNTRY  0
-#define MTRC_ID_CITY     1
+#define MTRC_ID_COUNTRY 0
+#define MTRC_ID_CITY 1
 #define MTRC_ID_HOSTNAME 2
 
 #include "commons.h"
@@ -45,12 +44,10 @@ typedef enum GAnonymizeLevels_ {
 } GAnonymizeLevels;
 
 /* Function Prototypes */
-GHolder *new_gholder (uint32_t size);
-void *add_hostname_node (void *ptr_holder);
-void free_holder_by_module (GHolder ** holder, GModule module);
-void free_holder (GHolder ** holder);
-void load_holder_data (GRawData * raw_data, GHolder * h, GModule module, GSort sort);
-void load_host_to_holder (GHolder * h, char *ip);
-int dup_key_list (void *val, GSLList ** user_data);
-
-#endif // for #ifndef GHOLDER_H
+GHolder* new_gholder(uint32_t size);
+void* add_hostname_node(void* ptr_holder);
+void free_holder_by_module(GHolder** holder, GModule module);
+void free_holder(GHolder** holder);
+void load_holder_data(GRawData* raw_data, GHolder* h, GModule module, GSort sort);
+void load_host_to_holder(GHolder* h, char* ip);
+int dup_key_list(void* val, GSLList** user_data);
