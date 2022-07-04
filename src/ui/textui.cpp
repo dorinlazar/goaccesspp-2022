727247902d5648b3d9c2cf720d2dcd701703d731
Dorin Lazăr <dorin.lazar@gmail.com>
2022-07-04 20:49:17 +0300

    window allocation proper

--- src/ui/textui.cpp
+++ src/ui/textui.cpp
@@ -1,10 +1,12 @@
 #include "textui.hpp"
 
 #include <cstring>
-
+#include <memory>
 #include "error.hpp"
 
 namespace goapp {
+
+static std::unique_ptr<TextWindow> s_screen;
 
 void TextUiSystem::Init(bool mouse_support) {
   initscr();
@@ -20,16 +22,35 @@ void TextUiSystem::Init(bool mouse_support) {
   if (mouse_support) {
     mousemask(BUTTON1_CLICKED, NULL);
   }
+  s_screen = std::make_unique<TextWindow>(stdscr);
 }
 
-void TextUiSystem::Close() { endwin(); }
+void TextUiSystem::Close() {
+  s_screen.reset();
+  endwin();
+}
 
-TextWindow::TextWindow(int x, int y, int width, int height) { m_win = newwin(width, height, x, y); }
+UISize TextUiSystem::ScreenSize() { return s_screen->Size(); }
+
+TextWindow::TextWindow(int x, int y, int width, int height) { m_win = newwin(height, width, y, x); }
+TextWindow::TextWindow(UICoords where, UISize size) { m_win = newwin(size.height, size.width, where.y, where.x); }
+TextWindow::TextWindow(WINDOW* wnd) {
+  m_win = wnd;
+  m_owned = false;
+}
 
 TextWindow::~TextWindow() {
   wclear(m_win);
   wrefresh(m_win);
-  delwin(m_win);
+  if (m_owned) {
+    delwin(m_win);
+  }
+}
+
+UISize TextWindow::Size() {
+  UISize size;
+  getmaxyx(m_win, size.height, size.width);
+  return size;
 }
 
 } // namespace goapp
--- src/ui/textui.hpp
+++ src/ui/textui.hpp
@@ -3,20 +3,35 @@
 #include <ncurses.h>
 namespace goapp {
 
+struct UISize {
+  int width = 0;
+  int height = 0;
+};
+
+struct UICoords {
+  int x = 0;
+  int y = 0;
+};
+
 class TextUiSystem {
 public:
   static void Init(bool mouse_support);
   static void Close();
+  static UISize ScreenSize();
 };
 
 class TextWindow {
   WINDOW* m_win;
+  bool m_owned = true;
 
 public:
   TextWindow(int x, int y, int width, int height);
+  TextWindow(UICoords, UISize);
+  TextWindow(WINDOW*);
   ~TextWindow();
 
   void KeyPad();
+  UISize Size();
 };
 
 } // namespace goapp
